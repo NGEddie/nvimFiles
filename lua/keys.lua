4f39e21c59fe3c4e86d908a5b4352d348a267ccf
Nigel Gormley <nigel@MacBookPro>
2022-12-18 16:15:32 +0000

    Added more key bindings (R)

--- lua/keys.lua
+++ lua/keys.lua
@@ -18,6 +18,16 @@ keymap('c', 'Q<cr>', 'q<cr>', opts)
 keymap('c', 'W<cr>', 'w<cr>', opts)
 keymap('c', 'Wq<cr>', 'wq<cr>', opts)
 
+-- Window/Pane Navigation Bindings
+keymap('n', '<leader>l', '<C-W><C-l>', opts)
+keymap('n', '<leader>h', '<C-W><C-h>', opts)
+keymap('n', '<leader>j', '<C-W><C-j>', opts)
+keymap('n', '<leader>k', '<C-W><C-k>', opts)
+keymap('n', '<leader><left>', ':vertical resize +15<cr>', opts)
+keymap('n', '<leader><right>', ':vertical resize -15<cr>', opts)
+keymap('n', '<leader><up>', ':resize +3<cr>', opts)
+keymap('n', '<leader><down>', ':resize -3<cr>', opts)
+
 -- Buffer Bindings
 keymap('n', '<leader>b', ':ls<cr>', opts)
 keymap('n', '<leader>bn', ':bn<cr>', opts)
@@ -66,3 +76,31 @@ vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
 vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float)
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist)
+
+-- Terminal Bindings
+keymap('n', '<leader>tt', ':botright 10sp<cr>:term<cr>i', {})
+keymap('n', '<leader>tv', ':botright 50vsp<cr>:term<cr>i', {})
+keymap('n', '<leader>tt', ':rightbelow sp<cr>:term<cr>i', {})
+keymap('n', '<leader>tt', ':vert sp<cr>:term<cr>i', {})
+
+-- NVIM-R Bindings
+keymap('n', '<leader>r<space>', ':call StartR("default")<cr><C-W><C-l><C-W>T', opts) -- Start R
+keymap('n', '<leader>rq', ':call RQuit("nosave")<cr>', opts) -- Close R
+keymap('n', '<leader>rp', ':call RAction("print")<cr>', opts) -- Print what ever is under cursor
+keymap('n', '<leader>rsf', ':call SendFileToR("silent")<cr>', opts) -- Send file to R
+keymap('n', '<leader>rsl', ':call SendLineToR("down")<cr>', opts) -- Send line to R and move down
+keymap('n', '<leader>rdf', ':call RAction("viewobj", ", howto=\'split\'")<cr>', opts)
+keymap('n', '<leader>ro', ':call RObjBrowser()<cr>', opts)
+-- nnoremap <silent> <leader>ro :call RObjBrowser()<cr>
+-- nnoremap <silent> <leader>rq :call RQuit('nosave')<cr> -- Close R
+-- nnoremap <silent> <leader>rr :call b:SendChunkToR("silent", "stay")<cr> -- Send Chunk to R
+-- nnoremap <silent> <leader>rsc :call b:SendChunkToR("silent", "stay")<cr>
+-- nnoremap <silent> <leader>rsl :call SendLineToR("down")<cr> -- Send line to R
+-- nnoremap <silent> <leader>rsf :call SendFileToR("silent")<cr> -- Send file to R (R files only)
+-- nnoremap <silent> <leader>rn :call RAction("nvim.names")<cr> -- get names/headers in data frame
+-- nnoremap <silent> <leader>rh :call RAction("head")<cr>
+-- nnoremap <silent> <leader>rp :call RAction("print")<cr>
+-- nnoremap <silent> <leader>rs :call RAction("str")<cr>
+-- nnoremap <silent> <leader>rdf :call RAction("viewobj", ", howto='split'")<cr>
+-- nnoremap <silent> <leader>ro :call RObjBrowser()<cr>
+
